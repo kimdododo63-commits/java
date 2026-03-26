2ab476c5fd44ebe38fc67568fe1806a1367a6e92
kim2 <kimdododo63@gmail.com>
2026-03-26 10:38:23 +0900

    Add files via upload

---
+++ S4_04.java
@@ -0,0 +1,23 @@
+package jump2java;
+
+import java.util.ArrayList;
+
+public class S4_04 {
+    public static void main(String[] args) {
+    	boolean hasCard = true;
+    	ArrayList<String> pocket = new ArrayList<String>();
+    	pocket.add("paper");
+    	pocket.add("handphone");
+
+    	if (pocket.contains("money")) {
+    	    System.out.println("택시를 타고 가라");
+    	}else if(hasCard) {
+    	    System.out.println("택시를 타고 가라");
+    	}else {         
+    	    System.out.println("걸어가라");
+    	}
+
+
+    }
+   
+}
---
+++ S4_06.java
@@ -0,0 +1,14 @@
+package jump2java;
+
+public class S4_06 {
+    public static void main(String[] args) {
+    	int treeHit = 0;
+    	while (treeHit < 10) {
+    	    treeHit++;  // treeHit += 1 로도 표현 가능
+    	    System.out.println("나무를  " + treeHit + "번 찍었습니다.");
+    	    if (treeHit == 10) {
+    	        System.out.println("나무 넘어갑니다.");
+    	    }
+    	}
+}
+}
